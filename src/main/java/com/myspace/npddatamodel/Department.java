bd7976732274f968eda3c10040a438a2eab0e149
abarfa <>
2020-06-11 10:38:16 -0400

    {/src/main/java/com/myspace/npddatamodel/Department.java}

--- src/main/java/com/myspace/npddatamodel/Department.java
+++ src/main/java/com/myspace/npddatamodel/Department.java
@@ -6,12 +6,33 @@ package com.myspace.npddatamodel;
 
 public class Department implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Department() {
-    }
+	private java.lang.Integer id;
+	private java.lang.String name;
 
+	public Department() {
+	}
 
+	public java.lang.Integer getId() {
+		return this.id;
+	}
 
+	public void setId(java.lang.Integer id) {
+		this.id = id;
+	}
+
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public Department(java.lang.Integer id, java.lang.String name) {
+		this.id = id;
+		this.name = name;
+	}
 
 }
